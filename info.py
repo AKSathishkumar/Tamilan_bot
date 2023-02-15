68d9562c8c4871fbb2ac95a607713b9ae00a5ce7
AKSathishkumar <117323286+AKSathishkumar@users.noreply.github.com>
2023-02-15 18:43:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aksathish:Ayyappa@ak@akmoviebot.2sc4sfb.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "akmoviebot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kavibot:Ayyappakavi@kavibot.0a1ubqd.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "kavibot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
